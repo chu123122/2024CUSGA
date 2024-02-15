0f0bacf9ce8f58d889c891dca2b95eb353aa3954
chu123122 <yorushika123122@163.com>
2024-02-15 22:14:48 +0800

    地面检测

---
+++ IsGroundCheck.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsGroundCheck : MonoBehaviour
+{
+    public static bool isGround;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGround = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGround = false;
+        }
+    }
+}
