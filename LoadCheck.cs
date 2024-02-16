ca47dcd623c9c0e37b3b927dd7118380843760c8
SkySakura2005 <160121026+SkySakura2005@users.noreply.github.com>
2024-02-16 14:31:21 +0800

    人物落地后的检测

--- LoadCheck.cs
+++ LoadCheck.cs
@@ -12,7 +12,7 @@ public class LoadCheck : MonoBehaviour
     private void Update()
     {
         loadCheck();
-        Debug.Log("isLoad:" + isLoad);
+        Debug.Log("isLoad:" + hasLoad);
         if(hasLoad){
             ;//执行着陆后的有关行为
             hasLoad=false;
