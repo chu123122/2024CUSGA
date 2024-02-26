baae74b2faba133aa858c46753cd93095e4b3ea4
chu123122 <yorushika123122@163.com>
2024-02-27 00:33:06 +0800

    碰到冲刺弹珠的持有状态

---
+++ State Machine System/Player States/PlayerState_DashCircle_1.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/StateMachine/PlayerState/Dash_1", fileName = "PlayerState_DashCircle_1")]
+public class PlayerState_DashCircle_1 : PlayerState
+{
+
+}
